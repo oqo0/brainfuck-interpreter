3b52e0203ae5f3dd82982b23ec89585076623e21
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-10-31 01:13:26 +0300

    Create interpreter basis

---
+++ brainfuck-interpreter/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace BrainfuckInterpreter
+{
+    class BrainfuckInterpreter
+    {
+        static void Main()
+        {
+            string commandBuffer = Console.ReadLine();
+            
+            int[] memory = Enumerable.Repeat(0, 30000).ToArray();
+            int pointer = 0;
+
+            foreach (var command in commandBuffer)
+            {
+                switch (command)
+                {
+                    case (','):
+                    {
+                        string input = Console.ReadLine();
+                        memory[pointer] = Convert.ToInt32(pointer);
+                        break;
+                    }
+                    case ('.'):
+                    {
+                        Console.WriteLine(memory[pointer]);
+                        break;
+                    }
+                    case ('>'):
+                    {
+                        if (pointer == memory.Length - 1)
+                        {
+                            pointer = 0;
+                            break;
+                        }
+                        
+                        pointer++;
+                        break;
+                    }
+                    case ('<'):
+                    {
+                        if (pointer == 0)
+                        {
+                            pointer = memory.Length - 1;
+                            break;
+                        }
+                        
+                        pointer--;
+                        break;
+                    }
+                    case ('+'):
+                    {
+                        memory[pointer]++;
+                        break;
+                    }
+                    case ('-'):
+                    {
+                        memory[pointer]--;
+                        break;
+                    }
+                    case ('['):
+                    {
+                        break;
+                    }
+                    case (']'):
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+}
