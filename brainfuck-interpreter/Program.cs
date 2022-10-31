8bca1e775edea347c84fde94ac5de486b27091aa
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-10-31 18:40:34 +0300

    Add code from file execution

--- brainfuck-interpreter/Program.cs
+++ brainfuck-interpreter/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Linq;
-
-namespace BrainfuckInterpreter
+﻿namespace BrainfuckInterpreter
 {
     class BrainfuckInterpreter
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Interpreter interpreter = new Interpreter(">+++++++++++++++[<+>>>>>>>>++++++++++<<<<<<<-]>+++++[<+++++++++>-]+>>>>>>+[<<+++[>>[-<]<[>]<-]>> [>+>]<[<]>]>[[->>>>+<<<<]>>>+++>-]<[<<<<]<<<<<<<<+[->>>>>>>>>>>>[<+[->>>>+<<<<]> >>>>]<<<<[>>>>>[<<<<+>>>>-]<<<<<-[<<++++++++++>>-]>>>[<<[<+<<+>>>-]<[>+<-]<++<<+ >>>>>>-]<<[-]<<-<[->>+<-[>>>]>[[<+>-]>+>>]<<<<<]>[-]>+<<<-[>>+<<-]<]<<<<+>>>>>>> >[-]>[<<<+>>>-]<<++++++++++<[->>+<-[>>>]>[[<+>-]>+>>]<<<<<]>[-]>+>[<<+<+>>>-]<<< <+<+>>[-[-[-[-[-[-[-[-[-<->[-<+<->>]]]]]]]]]]<[+++++[<<<++++++++<++++++++>>>>-]< <<<+<->>>>[>+<<<+++++++++<->>>-]<<<<<[>>+<<-]+<[->-<]>[>>.<<<<[+.[-]]>>-]>[>>.<< -]>[-]>[-]>>>[>>[<<<<<<<<+>>>>>>>>-]<<-]]>>[-]<<<[-]<<<<<<<<]++++++++++.");
-            string res = interpreter.GetOutput();
-            Console.WriteLine(res);
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+                
+            try
+            {
+                string code = File.ReadAllText(args[0]);
+
+                var interpreter = new Interpreter(code);
+                Console.WriteLine(interpreter.GetOutput());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid file name \\ path.");
+            }
+            finally
+            {
+                watch.Stop();
+                Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            }
         }
     }
 }
